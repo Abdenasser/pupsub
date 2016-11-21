ece506faff9017280202e024d1f102796df341f9
abdenasser <nasser.elidrissi065@gmail.com>
2016-11-21 19:56:50 +0000

    pushing badges to redis client

--- models/badges.js
+++ models/badges.js
@@ -11,5 +11,9 @@ var redis = require('../lib/redis');
  */
 
 exports.save = function(badges, callback) {
-
+  var badge = badges.pop();
+  redis.lpush('badges', JSON.stringify(badge), function(err) {
+    if (err) return callback(err, null);
+    exports.save(badges, callback);
+  });
 };
