7788d09dd942f82d2a39b1e988d53d99ea209682
abdenasser <nasser.elidrissi065@gmail.com>
2016-11-21 19:58:57 +0000

    stop the recursive callback when no more badges exist

--- models/badges.js
+++ models/badges.js
@@ -11,6 +11,7 @@ var redis = require('../lib/redis');
  */
 
 exports.save = function(badges, callback) {
+  if (!badges.length) return callback(null, null);
   var badge = badges.pop();
   redis.lpush('badges', JSON.stringify(badge), function(err) {
     if (err) return callback(err, null);
